210dd5e21fca634fdca044f207470bec3b396560
X01X012013 <xuhaiyang_1997@hotmail.com>
2017-04-30 12:22:24 -0600

    new archive look

--- Archive/main.js
+++ Archive/main.js
@@ -16,18 +16,6 @@ const initSystem = function (key, title) {
 };
 
 /**
- * All possible keys for database.
- * @const
- */
-const keys = ["chatting", "management", "issue", "offtopic", "coc"];
-
-/**
- * Element for each key, this array is a parallel array with {@link keys}.
- * @const
- */
-const elemID = ["listChatting", "listManagement", "listIssue", "listOffTopic", "listCOC"];
-
-/**
  * When the document is ready, draw archive pages, then check if the requested page exists.
  * If no page is requested, General Chatting Page 1 is loaded.
  * If the requested page does not exist, show an error message, otherwise, load the page.
@@ -35,8 +23,19 @@ const elemID = ["listChatting", "listManagement", "listIssue", "listOffTopic", "
  * @listens $(document).ready
  */
 $(document).ready(function () {
-    //Draw archive pages
+    //Constants
+    const keys = ["chatting", "management", "issue", "offtopic", "coc"];
+    const elemID = ["listChatting", "listManagement", "listIssue", "listOffTopic", "listCOC"];
+    const titles = ["General Chatting", "Alliance Management", "Report Issue", "Off-Topic", "Clash of Clans"];
+    //Crates a dropdown menu from template
+    const dropdownTemplate = function (title, id) {
+        return `<li class="dropdown"><a class ="dropdown-toggle" data-toggle="dropdown">${title}<span class ="caret"></span></a>` +
+               `<ul class="dropdown-menu" id="${id}"></ul></li>`;
+    };
+    //Draw archive list
     for (let i = 0; i < keys.length; i++) {
+        //Create dropdown menu
+        $("#listPlaceholder").before(dropdownTemplate(titles[i], elemID[i]));
         //Get pages
         const pages = db[keys[i]];
         //Check if we have at least one page
@@ -44,14 +43,29 @@ $(document).ready(function () {
             let page = null, j = 1;
             //Put pages into list div
             while (page = pages["page" + j.toString()]) {
-                $("#" + elemID[i]).append(`<p><a href="Archive.html?page=${keys[i]}-page${j}">${page[1]}</a></p>`);
+                $("#" + elemID[i]).append(`<li><a href="Archive.html?page=${keys[i]}-page${j}">Page ${j}</a></li>`);
                 j++;
             }
         } else {
             //No page at all, remove matching list div
-            $("#" + elemID[i]).parent().remove();
+            $("#" + elemID[i]).append(`<li class="disabled"><a>No archived page</a></li>`);
         }
     }
+    //Remove placeholder
+    $("#listPlaceholder").remove();
+    //Add dropdown animation
+    //http://stackoverflow.com/questions/12115833/adding-a-slide-effect-to-bootstrap-dropdown
+    $(".dropdown").on("show.bs.dropdown", function () {
+        $(".dropdown").not(this).trigger("hide.bs.dropdown");
+        $(this).find(".dropdown-menu").first().stop(true, true).slideDown(200);
+    });
+    $(".dropdown").on("hide.bs.dropdown", function (e) {
+        e.preventDefault();
+        $(this).find(".dropdown-menu").first().stop(true, true).slideUp(200, (function () {
+            $(this).removeClass("open");
+            $(this).find(".dropdown-toggle").attr("aria-expanded", "false");
+        }).bind(this));
+    });
     //Check requested page and load it
     let key, page;
     if ((/^\?page\=.*\-.*/).test(window.location.search)) {
