2efdc5f9ca38c0e36e80d180253e4b8b5fe2c58d
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-01-02 11:09:44 -0700

    move discord link to common script

--- common/common.js
+++ common/common.js
@@ -6,8 +6,13 @@
  * @listens $(document).ready
  */
 $(document).ready(function () {
+    // Set Discord link
+    const discordInviteKey = "VtnmVms";
+    const discordElement = document.getElementById("discordLink");
+    discordElement.href = "https://discord.gg/" + discordInviteKey;
+    discordElement.textContent = "discord.gg/" + discordInviteKey;
     // Set footer
-    $("#footer").text("Community Chat Board v2.2 by jspenguin2017");
+    $("#footer").text("Community Chat Board v2.2 by Hugo Xu");
     // Set jump to top button animation
     $("#jumpToTop").click(function (e) {
         $("html, body").animate({ scrollTop: 0 }, "fast");
